5b8f33f4675555595481f9cec01167ad476fb7ea
daulat <daulatjajra2006@gmail.com>
2024-01-01 15:48:33 +0530

    'function'

--- function.js
+++ function.js
@@ -1,2 +1,13 @@
 "use strict ";
 
+// Closures in js
+function outerFunction() {
+  let outerVariable = "I'm a outer variable";
+  function innerFunction() {
+    console.log(outerVariable);
+  }
+  return innerFunction();
+}
+
+let ClosureExample = outerFunction();
+ClosureExample();
