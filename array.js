73f8f34c0ea7184855fa0c2fafc87499a0e93e64
daulat <daulatjajra2006@gmail.com>
2024-01-02 21:08:54 +0530

    array

---
+++ array.js
@@ -0,0 +1,67 @@
+"use strict";
+
+/////////////////////////////////////////////////////
+// Array Methods
+
+// let arr = ["a", "b", "c", "d", "e", "f", "g"];
+
+// //1.  slice method - imuted (can't change the original array)
+// console.log(arr.slice(3));
+// console.log(arr.slice(3, 5)); // The end(5th) paremeter is not included in the output
+// console.log(arr.slice(1, -1));
+// console.log(arr.slice(-1));
+// console.log(arr.slice()); // A shallow copy of the array
+// console.log([...arr]); // Also a shallow copy of the array
+
+// // 2. Splice Method - Muted the array
+// console.log(arr.splice(3)); //(4) ['d', 'e', 'f', 'g']
+// console.log(arr.splice(1, 2)); //(2) ['b', 'c']
+// //(from delete, How many delete)
+// console.log(arr); //['a']  //The Original
+
+// // 3. reverse method : Muted the original array
+
+// let arr2 = ["n", "m", "l", "k", "j", "i", "h"];
+// console.log(arr2.reverse());
+// // console.log(arr2);
+
+// // 4. concat Method : Imuted the array
+// let letter = arr.concat(arr2);
+// console.log(letter); // all letter
+// console.log([...arr, ...arr2]); // the another way to concat
+
+// // 5. join method - Imuted the array
+// console.log(letter.join("-")); // Output will be the 'String'
+// console.log(letter);
+
+////////////////////////////////////////////////////////////////
+
+// // 6. at Method
+// let arr = [23, 12, 67, 90];
+// console.log(arr[0]);
+// console.log(arr.at(0));
+
+// // getting the last array element
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+
+// // also  worked for strings :
+// console.log("Daulat Jajra".at(0));
+
+/////////////////////////////////////////////////////////////////////////
+
+// 7. ForEach Method
+let movements = [200, 450, -400, 3000, -600, -130, 70, 1300];
+
+// for (let movement in movements) {
+//   movements[movement] > 0
+//     ? console.log(`You deposited ${movements[movement]}`)
+//     : console.log(`You withdrew ${Math.abs(movements[movement])}`);
+// }
+
+movements.forEach(function (movement) {
+  movement > 0
+    ? console.log(`You deposited ${movement}`)
+    : console.log(`You withdrew ${Math.abs(movement)}`);
+});
--- function.js
+++ function.js
@@ -1,65 +1,65 @@
 "use strict ";
 
-// Closures in js
-function outerFunction() {
-  let outerVariable = "I'm a outer variable";
-  function innerFunction() {
-    console.log(outerVariable);
-  }
-  return innerFunction();
-}
+// // Closures in js
+// function outerFunction() {
+//   let outerVariable = "I'm a outer variable";
+//   function innerFunction() {
+//     console.log(outerVariable);
+//   }
+//   return innerFunction();
+// }
 
-let buses = function () {
-  let bus = 0;
-  return function () {
-    bus += 1;
-    console.log(bus);
-  };
-};
-let busFun = buses();
-// busFun();
-// busFun();
+// let buses = function () {
+//   let bus = 0;
+//   return function () {
+//     bus += 1;
+//     console.log(bus);
+//   };
+// };
+// let busFun = buses();
+// // busFun();
+// // busFun();
 
-// More example with closures //////////////////////
-// We don't need to return function
-// Example - 1
-let f;
+// // More example with closures //////////////////////
+// // We don't need to return function
+// // Example - 1
+// let f;
 
-let g = function () {
-  let a = 12;
-  f = function () {
-    console.log(a * 2);
-  };
-};
+// let g = function () {
+//   let a = 12;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
 
-let h = function () {
-  let b = 144;
-  f = function () {
-    console.log(b * 2);
-  };
-};
+// let h = function () {
+//   let b = 144;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
 
-g();
-f();
-console.dir(f);
+// g();
+// f();
+// console.dir(f);
 
-// Re-assigning f function
-h();
-f();
-console.dir(f);
+// // Re-assigning f function
+// h();
+// f();
+// console.dir(f);
 
-// Example - 2  - Timer
+// // Example - 2  - Timer
 
-let boardPassanger = function (n, wait) {
-  let perGroup = n / 3;
+// let boardPassanger = function (n, wait) {
+//   let perGroup = n / 3;
 
-  setTimeout(function () {
-    console.log(`We are now boarding all ${n} passengers`);
-    console.log(`There are 3 groups, each with ${perGroup} passengers`);
-  }, wait * 1000);
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
 
-  console.log(`Will start boarding in ${wait} seconds`);
-};
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
 
-// let perGroup = 1000;
-boardPassanger(180, 3);
+// // let perGroup = 1000;
+// boardPassanger(180, 3);
