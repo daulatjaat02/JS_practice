3c8f8c5fc00b560e279ffc916f1d729268100af7
daulat <daulatjajra2006@gmail.com>
2024-01-02 09:08:25 +0530

    'closures'

--- function.js
+++ function.js
@@ -9,5 +9,42 @@ function outerFunction() {
   return innerFunction();
 }
 
-let ClosureExample = outerFunction();
-ClosureExample();
+let buses = function () {
+  let bus = 0;
+  return function () {
+    bus += 1;
+    console.log(bus);
+  };
+};
+let busFun = buses();
+// busFun();
+// busFun();
+
+// More example with closures //////////////////////
+
+// Example - 1
+let f;
+
+let g = function () {
+  let a = 12;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+let h = function () {
+  let b = 144;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+
+console.dir(f);
