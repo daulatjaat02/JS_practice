79ea42f3ff46d7d052400f830ec7aae2f4d7e84c
daulat <daulatjajra2006@gmail.com>
2024-01-05 09:50:35 +0530

    'filter'

--- array.js
+++ array.js
@@ -52,7 +52,7 @@
 /////////////////////////////////////////////////////////////////////////
 
 // // 7. ForEach Method
-// let movements = [200, 450, -400, 3000, -600, -130, 70, 1300];
+let movements = [200, 450, -400, 3000, -600, -130, 70, 1300];
 
 // // for (let movement in movements) {
 // for (const [i, movement] of movements.entries()) {
@@ -152,70 +152,127 @@
 // console.log(movementDescriptions);
 
 //////////////////////////////////////////
-// map() Examples
+// // map() Examples
 
-// 1. Doubling each element in an array
-let numbers = [1, 2, 3, 4, 5, 6, 7];
-let square = numbers.map((num) => num * num);
-console.log(square);
+// // 1. Doubling each element in an array
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+// let square = numbers.map((num) => num * num);
+// console.log(square);
 
-// 2. Converting strings to upperCase
-let names = ["Daulat", "Muskan", "Manisha", "Suman", "Puspa", "Yash"];
-let UpperCase = names.map((word) => word.toUpperCase());
-console.log(UpperCase);
+// // 2. Converting strings to upperCase
+// let names = ["Daulat", "Muskan", "Manisha", "Suman", "Puspa", "Yash"];
+// let UpperCase = names.map((word) => word.toUpperCase());
+// console.log(UpperCase);
 
-//3. Extracting values from an array of objects
+// //3. Extracting values from an array of objects
 
-let students = [
-  { name: "Daulat", age: 20, city: "Jaipur" },
-  { name: "Puspa", age: 25, city: "Udaipur" },
-  { name: "Manish", age: 20, city: "Jodhpur" },
+// let students = [
+//   { name: "Daulat", age: 20, city: "Jaipur" },
+//   { name: "Puspa", age: 25, city: "Udaipur" },
+//   { name: "Manish", age: 20, city: "Jodhpur" },
+// ];
+// let girls = [
+//   { name: "Keya", age: 20, city: "Jaipur" },
+//   { name: "Puspa", age: 25, city: "Udaipur" },
+//   { name: "Jublina", age: 20, city: "Jodhpur" },
+// ];
+
+// let studentsName = students.map((students) => students.name);
+// let girlsName = girls.map((students) => students.name);
+
+// console.log(studentsName);
+// console.log(girlsName);
+
+// // 4. Calculating square-roots
+// let squares = [9, 16, 25, 36, 49, 64];
+// let squareRoots = squares.map((square) => Math.sqrt(square));
+// console.log(squareRoots);
+
+// // 5. Using map with index parameter
+// let languages = ["JavaScript", "Python", "C", "C++", "Java", "PHP"];
+// let languagesIndex = languages.map((lang, i) => `${i + 1} - ${lang}`);
+// console.log(languagesIndex);
+
+// // 6. Mapping over an array of objects
+
+// let users = [
+//   { id: 1, name: "Daulat", age: 20 },
+//   { id: 2, name: "Puspa", age: 25 },
+//   { id: 3, name: "Manish", age: 20 },
+// ];
+
+// let ids = users.map((users) => users.id);
+// console.log(ids);
+
+// // 7. Transforming and filterin using map and filter
+
+// numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let SqaureEvens = numbers.filter((sum) => sum % 2 === 0).map((sum) => sum ** 2);
+// console.log(SqaureEvens);
+
+// // 8. using 'thisArg' to double values with a custom mulitplier
+
+// numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// let mulitplier = 3;
+// let multiplieNumbers = numbers.map(function (num) {
+//   return this * num;
+// }, mulitplier);
+// console.log(multiplieNumbers);
+
+// The filter method in js
+
+// let deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+// console.log(movements);
+// console.log(deposits);
+
+// // with for loop
+// let depositsfor = [];
+// for (let mov of movements) if (mov > 0) depositsfor.push(mov);
+// console.log(depositsfor);
+
+// let withdrewals = movements.filter(function (mov) {
+//   return mov < 0;
+// });
+// console.log(withdrewals);
+
+// Examples :
+
+// 1. filtering even numbers
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let evenNumbers = numbers.filter((num) => num % 2 === 0);
+console.log(evenNumbers);
+
+// filtering name starting with D
+let names = [
+  "David",
+  "Dayal",
+  "Priya",
+  "Dipak",
+  "Puspa",
+  "Daulat",
+  "Pratik",
+  "Dulal",
+  "Denaas",
+  "Muskaan",
 ];
-let girls = [
-  { name: "Keya", age: 20, city: "Jaipur" },
-  { name: "Puspa", age: 25, city: "Udaipur" },
-  { name: "Jublina", age: 20, city: "Jodhpur" },
+
+let Dnames = names.filter((name) => name.charAt(0) === "D");
+console.log(Dnames);
+
+// Filtering objects with a certain property value
+
+let products = [
+  { product: "Gun", price: 1200 },
+  { product: "Laptop", price: 500 },
+  { product: "Watch", price: 100 },
+  { product: "Phone", price: 800 },
+  { product: "Book", price: 120 },
 ];
 
-let studentsName = students.map((students) => students.name);
-let girlsName = girls.map((students) => students.name);
+let affordableProducts = products.filter((products) => products.price < 500);
+console.log(affordableProducts);
 
-console.log(studentsName);
-console.log(girlsName);
-
-// 4. Calculating square-roots
-let squares = [9, 16, 25, 36, 49, 64];
-let squareRoots = squares.map((square) => Math.sqrt(square));
-console.log(squareRoots);
-
-// 5. Using map with index parameter
-let languages = ["JavaScript", "Python", "C", "C++", "Java", "PHP"];
-let languagesIndex = languages.map((lang, i) => `${i + 1} - ${lang}`);
-console.log(languagesIndex);
-
-// 6. Mapping over an array of objects
-
-let users = [
-  { id: 1, name: "Daulat", age: 20 },
-  { id: 2, name: "Puspa", age: 25 },
-  { id: 3, name: "Manish", age: 20 },
-];
-
-let ids = users.map((users) => users.id);
-console.log(ids);
-
-// 7. Transforming and filterin using map and filter
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-let SqaureEvens = numbers.filter((sum) => sum % 2 === 0).map((sum) => sum ** 2);
-console.log(SqaureEvens);
-
-// 8. using 'thisArg' to double values with a custom mulitplier
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-let mulitplier = 3;
-let multiplieNumbers = numbers.map(function (num) {
-  return this * num;
-}, mulitplier);
-console.log(multiplieNumbers);
+// filtering based on index
