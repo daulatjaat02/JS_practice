ffc404384a1839e2e4d312b7c844463afbaf0e0e
daulat <daulatjajra2006@gmail.com>
2024-01-11 23:55:45 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// selectors :
+console.log(document.documentElement);
+console.log(document.body);
+console.log(document.head);
+
+console.log(document.querySelector(".container"));
+console.log(document.querySelectorAll(".container"));
+
+console.log(document.getElementById("container"));
+console.log(document.getElementsByTagName("div"));
+console.log(document.getElementsByClassName("container"));
