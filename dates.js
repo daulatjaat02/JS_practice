63eccc0836b532d9f3eaf0734bf5bfd575d89e46
daulat <daulatjajra2006@gmail.com>
2024-01-19 20:15:16 +0530

    dates

--- dates.js
+++ dates.js
@@ -7,3 +7,15 @@
 //   Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
 // let days = calcDaysPassed(new Date(2024, 1, 23), new Date(2024, 1, 12));
 // console.log(days);
+
+///////////////////////////////////////////////////////////////////////////////
+// Chapter 1 : Converting and checking numbers
+
+// let decimalNumber = 26;
+// let binaryEquivalent = decimalNumber.toString(2);
+// console.log(binaryEquivalent); // Output: "11010"
+
+// console.log(isNaN("daulat")); // true
+// console.log(isNaN(26)); // false
+let ceiledNumber = Math.ceil(4.2);
+console.log(ceiledNumber); // Output: 5
